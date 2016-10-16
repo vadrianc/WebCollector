dcfbb335cf755217755e80326a1436e7f3d46daf
Adrian Constantin Vaideanu <vaideanu.adrian@gmail.com>
2016-10-16 22:22:16 +0300

    Define classes for the Navigate and Collect actions and for the session.

---
+++ WebCollector/Actions/CollectTag.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using SoftwareControllerApi.Action;
+
+namespace WebCollector.Actions
+{
+    /// <summary>
+    /// Action for collecting the content of a tag.
+    /// </summary>
+    public class CollectTagAction : ICollectAction
+    {
+        /// <summary>
+        /// The name of the action.
+        /// </summary>
+        public string Name
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Execute the collect action.
+        /// </summary>
+        /// <returns>The result in which the content of the found tag is stored.</returns>
+        public IResult Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WebCollector/Actions/NavigateAction.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using SoftwareControllerApi.Action;
+
+namespace WebCollector.Actions
+{
+    /// <summary>
+    /// Action for navigating to a web page.
+    /// </summary>
+    public class NavigateAction : INavigateAction
+    {
+        /// <summary>
+        /// Get the link to navigate to.
+        /// </summary>
+        public string Link
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// The name of the action.
+        /// </summary>
+        public string Name
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Execute the navigation action.
+        /// </summary>
+        /// <returns>The result of the navigation action.</returns>
+        public IResult Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
