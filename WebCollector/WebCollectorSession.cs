f2a43c67991992ca19238a7a590707b85b60571e
Adrian Constantin Vaideanu <vaideanu.adrian@gmail.com>
2016-10-17 21:32:44 +0300

    Create the WebCollectorSession that derives from the Session class.

---
+++ WebCollector/WebCollectorSession.cs
@@ -0,0 +1,29 @@
+﻿using SoftwareControllerLib.Control;
+
+namespace WebCollector
+{
+    /// <summary>
+    /// The Web Collector session.
+    /// </summary>
+    public class WebCollectorSession : Session
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WebCollectorSession"/> class using the given name and web address.
+        /// </summary>
+        /// <param name="name">The name of the web collector session.</param>
+        /// <param name="address">The web address.</param>
+        public WebCollectorSession(string name, string address) : base(name)
+        {
+            Address = address;
+        }
+
+        /// <summary>
+        /// The address of the website from which data will be collected.
+        /// </summary>
+        public string Address
+        {
+            get;
+            private set;
+        }
+    }
+}
