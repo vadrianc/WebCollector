2a06553b30bee08dace54b5b1c1557eff9e37f82
Adrian Constantin Vaideanu <vaideanu.adrian@gmail.com>
2016-10-23 22:21:23 +0300

    Implement main program.

--- WebCollector/Program.cs
+++ WebCollector/Program.cs
@@ -1,9 +1,33 @@
-﻿namespace WebCollector
+﻿using System;
+using System.Net;
+using WebCollector.Config;
+using WebCollector.Utils;
+
+namespace WebCollector
 {
+    /// <summary>
+    /// Main program class.
+    /// </summary>
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            try {
+                WebConfigReader reader = new WebConfigReader("default.xml");
+                WebCollectorSession session = reader.Read();
+
+                session.Html = HtmlUtils.GetHtmlString(session.StartAddress);
+                session.Run();
+            } catch (WebException ex) {
+                Console.WriteLine(ex.Message);
+            } catch (Exception ex) {
+                Console.WriteLine("Fatal exception");
+                Console.WriteLine(ex.Message);
+            } finally {
+                Console.WriteLine();
+                Console.WriteLine("Press any key to exit ...");
+                Console.ReadKey();
+            }
         }
     }
 }
